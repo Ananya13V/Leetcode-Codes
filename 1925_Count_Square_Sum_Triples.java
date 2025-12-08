08286d088cd16261f958360789830856ef47c962
Ananya Verma <113530075+Ananya13V@users.noreply.github.com>
2025-12-08 04:43:00 +0000

    8th dec edits

--- 1925_Count_Square_Sum_Triples.java
+++ 1925_Count_Square_Sum_Triples.java
@@ -26,3 +26,22 @@ class Solution {
     }
 }
 
+// Optimisations: 
+// 1. Don't need data structure, can be done using number itself
+// 2. Don't need to use double
+// 3. Only 1 for loop is enough
+
+// Checked Solution
+// class Solution {
+//     public int countTriples(int n) {
+// int ans=0;
+// for(int i=1;i<n-1;i++){
+//     for(int j=i+1;j<n;j++){
+//         int req=(int)Math.sqrt(i*i+j*j);
+//         if((req*req)==(i*i+j*j) && req<=n) ans+=2;
+   
+//     }
+// } 
+// return ans;
+//     }
+// }
